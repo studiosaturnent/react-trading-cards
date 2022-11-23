5ee8b167c7e4f3e6630929418af08ce8a229d5bb
Simone Lawson <simonerlawson@gmail.com>
2022-11-23 11:55:39 -0500

    first commit

---
+++ static/js/tradingCard.jsx
@@ -0,0 +1,93 @@
+'use strict';
+
+const tradingCardData = [
+  {
+    name: 'Balloonicorn',
+    skill: 'video games',
+    imgUrl: '/static/img/balloonicorn.jpg',
+  },
+];
+
+function TradingCard(props) {
+  return (
+    <div className="card">
+      <h2>Name: {props.name}</h2>
+      <img src={props.imgUrl} alt="profile" />
+      <h2>Skill: {props.skill} </h2>
+    </div>
+  );
+}
+
+const tradingCardData2 = [
+  {
+    name: "Balloonicorn",
+    skill: "video games",
+    imgUrl: "/static/img/balloonicorn.jpg",
+    cardId: 1,
+  },
+  {
+    name: "Float",
+    skill: "baking pretzels",
+    imgUrl: "/static/img/float.jpg",
+    cardId: 2,
+  },
+  {
+    name: "Llambda",
+    skill: "knitting scarves",
+    imgUrl: "/static/img/llambda.jpg",
+    cardId: 3,
+  },
+  {
+    name: "Off-By-One",
+    skill: "climbing mountains",
+    imgUrl: "/static/img/off-by-one.jpeg",
+    cardId: 4,
+  },
+  {
+    name: "Seed.py",
+    skill: "making curry dishes",
+    imgUrl: "/static/img/seedpy.jpeg",
+    cardId: 5,
+  },
+  {
+    name: "Polymorphism",
+    skill: "costumes",
+    imgUrl: "/static/img/polymorphism.jpeg",
+    cardId: 6,
+  },
+  {
+    name: "Short Stack Overflow",
+    skill: "ocean animal trivia",
+    imgUrl: "/static/img/shortstack-overflow.jpeg",
+    cardId: 7,
+  },
+  {
+    name: "Merge",
+    skill: "bullet journaling",
+    imgUrl: "/static/img/merge.png",
+    cardId: 8,
+  },
+];
+console.log(tradingCardData2);
+
+ReactDOM.render(
+  <TradingCard name="Balloonicorn" skill="video games" imgUrl="/static/img/balloonicorn.jpg" />,
+  document.querySelector('#balloonicorn'),
+);
+
+ReactDOM.render(
+  <TradingCard name="Float" skill="baking pretzels" imgUrl="/static/img/float.jpg" />,
+  document.querySelector('#float'),
+);
+
+ReactDOM.render(
+  <TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
+  document.querySelector('#llambda'),
+);
+
+ReactDOM.render(
+  <TradingCard name="Pookie" skill ="rock throwing" imgUrl="/static/img/polymorphism.jpeg" />,
+  document.querySelector('#pookie'),
+);
+
+
